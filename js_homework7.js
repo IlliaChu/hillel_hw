545f50d1ec4274a7ec98a5d0d88bb8f19379ff70
chu <eliazmort@gmail.com>
2025-08-23 18:26:14 +0300

    Complete HW 10.1. Параноя

--- js_homework7.js
+++ js_homework7.js
@@ -15,4 +15,51 @@ function Exceptor(words) {
     }
     return result; // повертаємо те що вийшло
 }
-console.log(Exceptor(words)); 
+console.log("HW 10.2. Без А: " + Exceptor(words));
+
+
+
+//HW 10.1. Параноя
+
+var arr = [
+    {
+        userName:"Test",
+        lastName:"Test",
+        email:"test.test@gmail.com"
+    },
+    {
+        userName:"Dmitro",
+        lastName:"Porohov",
+        email:"dmitro.porohov@yahoo.com"
+        },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"andrii@mail.ru" // Нам такі не підходять
+    },
+];
+
+
+function mail_validation() {
+    let result = [];
+    let index = 0;
+    let regex = /^[A-Za-z0-9._-]+@[A-Za-z0-9]+\.com/; //окремо виніс для зручності корегування + домен .com
+    // let regex = /^[A-Za-z0-9._-]+@[A-Za-z0-9]+\.[A-Za-z0-9]{2,3}/; //регекс на кількість символів домену, не тільки .com
+
+
+    for (let key of arr) { // прохід по масиву
+        let email_check = key.email // додав у змінну key.email для використання .endsWith
+        let regex_block =
+            email_check.endsWith('.ru') ||
+            email_check.endsWith('yandex.ru') ||
+            email_check.endsWith('yandex.com') // блокуємо вивід з закінченнями
+
+        if (regex.test(email_check) && !regex_block) { // перевіряємо регекс + виключення
+            result[index] = email_check;
+            index++;
+        }
+    }
+    return result;
+}
+
+console.log("HW 10.1. Параноя:\n", mail_validation());
